6e79add2e5c52a7489fb537bcd9a3939d3b91647
hacker167 <iit2015045@iiita.ac.in>
2017-04-09 14:09:05 +0530

    Add files via upload

---
+++ PPLq4/gift.py
@@ -0,0 +1,8 @@
+class Gift :
+        """constructor for class Gift having name,price,value and type as attribute"""
+        """other attributes like access,rating also exist but is not in use for q1 and q2"""
+	def __init__(self,name_id,price,val,type_):
+		self.name_id = name_id
+		self.price = price
+		self.type_ = type_
+		self.val = val
