d86a8e7242740b1d00af5e414407384232e52264
hacker167 <iit2015045@iiita.ac.in>
2017-04-10 08:12:41 +0530

    Delete log.py

--- PPLq5/log.py
+++
@@ -1,9 +0,0 @@
-import logging
-"""create log file inputting file name,date ,message etc"""
-def log_maker(write) :
-	logging.basicConfig(format='%(asctime)s %(name)-6s %(levelname) s: %(message)s',
-					datefmt='%d/%m/%Y %I:%M:%S %p',
-					level=logging.DEBUG,
-                    filename='log.txt',
-                    filemode='w')
-	logging.info(write)
