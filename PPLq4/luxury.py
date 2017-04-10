80ecef9186672bc00a0672995d2e7b0eb6222b52
hacker167 <iit2015045@iiita.ac.in>
2017-04-10 08:06:41 +0530

    Delete luxury.py

--- PPLq4/luxury.py
+++
@@ -1,8 +0,0 @@
-from gift import Gift
-
-class Luxury(Gift):
-      def __init__(self,name_id,price,val,type_,rating,eco):
-                """constructor"""
-                Gift.__init__(self,name_id,price,val,type_)
-                self.rating=rating
-                self.eco=eco
