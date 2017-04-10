fb2c746848c6f7c929d0de108677b5a76182fee6
hacker167 <iit2015045@iiita.ac.in>
2017-04-10 08:44:11 +0530

    Add files via upload

---
+++ PPLq3/miser.py
@@ -0,0 +1,4 @@
+from boy import Boy
+class Miser(Boy):
+	def __init__(self,name_id,atr,bgt,intlg,min_attr,t):
+		Boy.__init__(self,name_id,atr,bgt,intlg,min_attr,t)
---
+++ PPLq3/normal.py
@@ -0,0 +1,6 @@
+from girl import Girl
+
+class normal(Girl):
+	def __init__(self,name_id,attr,main_bgt,intlg,t):
+		"""constructor"""
+		Girl.__init__(self,name_id,attr,main_bgt,intlg,t)
